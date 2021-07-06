456ee9a2c55d76d098421f696bdbf0bcf4d8cc27
cyberprogr <86689642+cyberprogr@users.noreply.github.com>
2021-07-06 03:16:47 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,200 @@
+
+
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+
+  <meta charset="UTF-8">
+  
+<link rel="apple-touch-icon" type="image/png" href="https://cpwebassets.codepen.io/assets/favicon/apple-touch-icon-5ae1a0698dcc2402e9712f7d01ed509a57814f994c660df9f7a952f3060705ee.png" />
+<meta name="apple-mobile-web-app-title" content="CodePen">
+
+<link rel="shortcut icon" type="image/x-icon" href="https://cpwebassets.codepen.io/assets/favicon/favicon-aec34940fbc1a6e787974dcd360f2c6b63348d4b1f4e06c77743096d55480f33.ico" />
+
+<link rel="mask-icon" type="" href="https://cpwebassets.codepen.io/assets/favicon/logo-pin-8f3771b1072e3c38bd662872f6b673a722f4b3ca2421637d5596661b4e2132cc.svg" color="#111" />
+
+
+  <title>Alshamal Trading</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
+  
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/tailwind.min.css'>
+  
+  <link rel="stylesheet" type="text/css" href="css/st.css">
+
+  <script>
+  window.console = window.console || function(t) {};
+</script>
+
+  
+  
+  <script>
+  if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage("resize", "*");
+  }
+</script>
+
+
+</head>
+
+<body translate="no" >
+  <nav class="main-nav w-full h-16">
+  <div class="container">
+    <div class="flex items-center h-16">
+      <div class="flex items-center w-full">
+        <div class="flex-shrink-0 z-50">
+          <a class="main-nav-logo" href="javascript:;">
+            <img alt="Molly" src="img/logo.png">
+        
+          </a>
+        </div>
+        <div class="flex ml-auto">
+          <div class="select-wrapper">
+            <div class="select-prepend">
+              <svg aria-hidden="true" class="w-4 h-4" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path fill="currentColor" d="M8 256c0 136.966 111.033 248 248 248s248-111.034 248-248S392.966 8 256 8 8 119.033 8 256zm248 184V72c101.705 0 184 82.311 184 184 0 101.705-82.311 184-184 184z"></path>
+              </svg>
+            </div>
+            <label class="sr-only" for="theme-select">Set Theme</label>
+            <select name="theme" id="theme-select">
+            </select>
+            <div class="select-append">
+              <svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-4 h-4">
+                <path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
+              </svg>
+            </div>
+          </div>
+          <button class="menu-btn"><svg class="block h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <g class="menu">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+              </g>
+              <g class="close-menu">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+              </g>
+            </svg></button>
+          <div class="menu-dropdown">
+            <ul>
+              <li>
+                <a href="javascript:;">Home </a>
+              </li>
+              <li>
+                <a href="javascript:;">Services</a>
+              </li>
+              <li>
+                <a href="javascript:;">OUR PRODUCTS</a>
+              </li>
+              <li>
+                <a href="javascript:;">OUR PRODUCTS</a>
+              </li>
+              <li>
+                <a href="javascript:;">About us</a>
+              </li>
+              <li>
+                <a href="javascript:;">CONTACT US</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+<main>
+  <div class="container">
+    <h3 class="page-title"> Services A professional group with dynamism, flexibility, team spirit, and ethical standards.
+</h3>
+    <div class="card-container p-4 rounded-xl md:p-8">
+      <div class="
+      grid
+      gap-4
+      grid-cols-1
+      sm:grid-cols-2
+      md:gap-8
+      lg:grid-cols-3
+      xl:grid-cols-4
+    ">
+        <div class="group homepage-card relative p-4 w-full rounded-lg">
+          <div class="card-img-wrapper rounded-md">
+              <!--<img src="https://dhs1n389ze6jv.cloudfront.net/img/product-thumb/1521431900460b50b418b0b56d17ceb711c6d74a5d.png" alt="" />-->
+            <div class="card-text-wrapper">
+              <a href="javascript:;" class="homepage-card-link">Objectives</a>
+              <p class="homepage-card-text">
+              Committed to major performance objectives in catering to the vital needs of the nation, with a high degree of dedication and
+									appetite for excellence and quality
+              </p>
+            </div>
+          </div>
+
+        </div>
+        <div class="group homepage-card relative p-4 w-full rounded-lg">
+          <div class="card-img-wrapper rounded-md">
+            <div class="card-text-wrapper">
+            <i class="fas fa-comments fa-6x"></i>
+
+              <a href="javascript:;" class="homepage-card-link">Activities</a>
+              <p class="homepage-card-text">
+              Our success has depended upon trade finance skills and specialized in managing risks. We have the reputation in devising secure innovative solutions.
+									We are proud of the tremendous market credibility among our customers and public at large              </p>
+            </div>
+          </div>
+
+        </div>
+     
+       
+        <div class="group homepage-card relative p-4 w-full rounded-lg">
+          <div class="card-img-wrapper rounded-md">
+            <div class="card-text-wrapper">
+            <i class="fas fa-comments fa-6x"></i>
+
+              <a href="javascript:;" class="homepage-card-link">Credibility</a>
+              <p class="homepage-card-text">
+              Our success has depended upon trade finance skills and specialized in managing risks. We have the reputation in devising secure innovative solutions.
+									We are proud of the tremendous market credibility among our customers and public at large             </p>
+            </div>
+          </div>
+
+        </div>
+     
+        
+        
+        
+        
+      </div>
+    </div>
+  </div>
+</main>
+<footer class="homepage-footer">
+  <div class="container">
+    <div class="flex justify-center pt-8">
+      <a class="morph-btn" href="https://github.com/AndrewHeinke" target="_blank" rel="noopener noreferrer">
+        <svg viewBox="0 0 16 16" aria-hidden="true">
+          <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+        </svg>
+      </a>
+      <a class="morph-btn" href="https://www.linkedin.com/in/andrewheinke/" target="_blank" rel="noopener noreferrer">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 34">
+          <path d="M34 2.5v29a2.5 2.5 0 01-2.5 2.5h-29A2.5 2.5 0 010 31.5v-29A2.5 2.5 0 012.5 0h29A2.5 2.5 0 0134 2.5zM10 13H5v16h5zm.45-5.5a2.88 2.88 0 00-2.86-2.9H7.5a2.9 2.9 0 000 5.8 2.88 2.88 0 002.95-2.81zM29 19.28c0-4.81-3.06-6.68-6.1-6.68a5.7 5.7 0 00-5.06 2.58h-.14V13H13v16h5v-8.51a3.32 3.32 0 013-3.58h.19c1.59 0 2.77 1 2.77 3.52V29h5z" />
+        </svg>
+      </a>
+      <a class="morph-btn" href="https://www.instagram.com/andrewheinke/" target="_blank" rel="noopener noreferrer">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 169.063 169.063">
+          <path d="M122.406 0H46.654C20.929 0 0 20.93 0 46.655v75.752c0 25.726 20.929 46.655 46.654 46.655h75.752c25.727 0 46.656-20.93 46.656-46.655V46.655C169.063 20.93 148.133 0 122.406 0zm31.657 122.407c0 17.455-14.201 31.655-31.656 31.655H46.654C29.2 154.063 15 139.862 15 122.407V46.655C15 29.201 29.2 15 46.654 15h75.752c17.455 0 31.656 14.201 31.656 31.655v75.752z" />
+          <path d="M84.531 40.97c-24.021 0-43.563 19.542-43.563 43.563 0 24.02 19.542 43.561 43.563 43.561s43.563-19.541 43.563-43.561c0-24.021-19.542-43.563-43.563-43.563zm0 72.123c-15.749 0-28.563-12.812-28.563-28.561 0-15.75 12.813-28.563 28.563-28.563s28.563 12.813 28.563 28.563c0 15.749-12.814 28.561-28.563 28.561zM129.921 28.251c-2.89 0-5.729 1.17-7.77 3.22a11.053 11.053 0 00-3.23 7.78c0 2.891 1.18 5.73 3.23 7.78 2.04 2.04 4.88 3.22 7.77 3.22 2.9 0 5.73-1.18 7.78-3.22 2.05-2.05 3.22-4.89 3.22-7.78 0-2.9-1.17-5.74-3.22-7.78-2.04-2.05-4.88-3.22-7.78-3.22z" />
+        </svg>
+      </a>
+    </div>
+  </div>
+</footer>
+    <script src="https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-8216c69d01441f36c0ea791ae2d4469f0f8ff5326f00ae2d00e4bb7d20e24edb.js"></script>
+
+  
+    <script src="js/cu.js"></script>
+
+
+  
+
+</body>
+
+</html>
+ 
